ad9138c7269bae6732eaaeb37a6fa40d848a10cf
LouisPetrik <louispetrik@aol.com>
2020-12-10 12:41:11 +0100

    readline added

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout,
+})
 
-console.log('Our CLI-tool!')
+readline.question(`What's your firstname? \n`, (fn) => {
+	readline.question(`What's your lastname? \n`, (ln) => {
+		console.log(`Hey ${fn} ${ln}!`)
+		readline.close()
+	})
+})
