74fd73ded9cc0bbc6e02a6920ed74960ef45e19b
LouisPetrik <louispetrik@aol.com>
2020-12-10 13:02:32 +0100

    added styling for console.log

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
+const chalk = require('chalk')
 const readline = require('readline').createInterface({
 	input: process.stdin,
 	output: process.stdout,
 })
 
-readline.question(`What's your firstname? \n`, (fn) => {
-	readline.question(`What's your lastname? \n`, (ln) => {
-		console.log(`Hey ${fn} ${ln}!`)
+readline.question(`What's your ` + chalk.blue('firstname') + `? \n`, (fn) => {
+	readline.question(`What's your ` + chalk.blue('lastname') + `? \n`, (ln) => {
+		console.log(chalk.green(`Hey ${fn} ${ln}!`))
 		readline.close()
 	})
 })
